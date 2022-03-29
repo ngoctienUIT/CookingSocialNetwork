e1d49d828af152fd9e96d49171245800c85258b7
AnBuiii <89350086+AnBuiii@users.noreply.github.com>
2022-03-29 18:05:55 +0700

    Update UI PostPage

--- app/src/main/java/com/example/cookingsocialnetwork/post/PostPage.kt
+++ app/src/main/java/com/example/cookingsocialnetwork/post/PostPage.kt
@@ -2,11 +2,13 @@ package com.example.cookingsocialnetwork.post
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 import com.example.cookingsocialnetwork.R
 
 class PostPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_post_page)
+        supportActionBar?.hide()
     }
 }
--- app/src/main/java/com/example/cookingsocialnetwork/post/PostViewModel.kt
+++ app/src/main/java/com/example/cookingsocialnetwork/post/PostViewModel.kt
@@ -1,6 +1,8 @@
 package com.example.cookingsocialnetwork.post
 
 import androidx.lifecycle.ViewModel
+import com.example.cookingsocialnetwork.R
 
 class PostViewModel:ViewModel() {
+
 }
