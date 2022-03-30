91ca80a02f53328fcefefeecbf8047e28c7d6439
AnBuiii <89350086+AnBuiii@users.noreply.github.com>
2022-03-30 19:45:17 +0700

    Update UI Setting

--- app/src/main/java/com/example/cookingsocialnetwork/main/fragment/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/cookingsocialnetwork/main/fragment/profile/ProfileFragment.kt
@@ -2,6 +2,7 @@ package com.example.cookingsocialnetwork.main.fragment.profile
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -18,6 +19,7 @@ class ProfileFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
+
     ): View {
         val binding: FragmentProfileBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_profile, container,false)
         val factory = ProfileViewModelFactory()
@@ -25,11 +27,14 @@ class ProfileFragment : Fragment() {
         binding.viewmodel = viewModel
         binding.lifecycleOwner = this
         return binding.root
+
     }
+
 
     fun startSettingChangeProfile()
     {
         val intent = Intent (activity, SettingChangeProfile::class.java)
         activity?.startActivity(intent)
     }
+
 }
--- app/src/main/java/com/example/cookingsocialnetwork/post/PostPage.kt
+++ app/src/main/java/com/example/cookingsocialnetwork/post/PostPage.kt
@@ -1,14 +1,20 @@
 package com.example.cookingsocialnetwork.post
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ImageButton
 import com.example.cookingsocialnetwork.R
+import com.example.cookingsocialnetwork.setting.SettingPage
+import kotlinx.android.synthetic.main.activity_post_page.*
 
 class PostPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_post_page)
         supportActionBar?.hide()
+        food_image.setOnClickListener{
+            val setting = Intent(this, SettingPage::class.java)
+            startActivity(setting)
+        }
     }
 }
