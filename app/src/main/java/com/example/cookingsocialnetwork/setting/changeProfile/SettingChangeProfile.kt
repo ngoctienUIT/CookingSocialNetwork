e99e876c76eb47fd60f71be38bce3221a1fe9711
Trần Ngọc Tiến <94039133+ngoctienUIT@users.noreply.github.com>
2022-05-10 19:18:45 +0700

    update setting profile
    cập nhật giao diện UI

--- app/src/main/java/com/example/cookingsocialnetwork/setting/changeProfile/SettingChangeProfile.kt
+++ app/src/main/java/com/example/cookingsocialnetwork/setting/changeProfile/SettingChangeProfile.kt
@@ -1,10 +1,15 @@
 package com.example.cookingsocialnetwork.setting.changeProfile
 
+import android.annotation.SuppressLint
+import android.app.DatePickerDialog
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import androidx.activity.result.contract.ActivityResultContract
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
@@ -19,12 +24,11 @@ import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
 import com.yalantis.ucrop.UCrop
 import java.io.File
-
+import java.util.*
 
 class SettingChangeProfile : AppCompatActivity() {
     private lateinit var viewModel: SettingChangeProfileViewModel
     private lateinit var dataBinding: ActivitySettingChangeProfileBinding
-
 
     private var getContent = registerForActivityResult(ActivityResultContracts.GetContent())
     {
@@ -42,7 +46,6 @@ class SettingChangeProfile : AppCompatActivity() {
             val uCrop = UCrop.of(inputUri!!, outputUri!!)
                 .withAspectRatio(5f, 5f)
                 .withMaxResultSize(800, 800)
-            Log.w("Có1", "Có mà")
 
             return uCrop.getIntent(context)
         }
@@ -69,6 +72,7 @@ class SettingChangeProfile : AppCompatActivity() {
         getContent.launch("image/*")
     }
 
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_setting_change_profile)
@@ -86,9 +90,35 @@ class SettingChangeProfile : AppCompatActivity() {
             finish()
         }
 
-        dataBinding.avatarBtn.setOnClickListener()
+        dataBinding.ivAvatar.setOnClickListener()
         {
             selectGallery()
+        }
+
+        dataBinding.birthday.setOnClickListener()
+        {
+            val c = Calendar.getInstance()
+            val year = c.get(Calendar.YEAR)
+            val month = c.get(Calendar.MONTH)
+            val day = c.get(Calendar.DAY_OF_MONTH)
+            val datePickerDialog = DatePickerDialog(this,
+                { _, newYear, monthOfYear, dayOfMonth ->
+                    dataBinding.birthday.text = "$dayOfMonth/${monthOfYear + 1}/$newYear"
+                }, year, month, day
+            )
+            datePickerDialog.show()
+        }
+
+        val adapter: ArrayAdapter<CharSequence> = ArrayAdapter.createFromResource(this, R.array.gender, android.R.layout.simple_spinner_item)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        dataBinding.gender.adapter = adapter
+        dataBinding.gender.onItemSelectedListener = object :AdapterView.OnItemSelectedListener
+        {
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                var gender: String = p0?.getItemAtPosition(p2).toString();
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {}
         }
     }
 
--- app/src/main/java/com/example/cookingsocialnetwork/setting/changeProfile/SettingChangeProfileViewModel.kt
+++ app/src/main/java/com/example/cookingsocialnetwork/setting/changeProfile/SettingChangeProfileViewModel.kt
@@ -3,89 +3,56 @@ package com.example.cookingsocialnetwork.setting.changeProfile
 import android.text.Editable
 import android.text.TextWatcher
 import androidx.lifecycle.ViewModel
+import com.example.cookingsocialnetwork.model.data.User
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import java.util.*
 
 class SettingChangeProfileViewModel: ViewModel() {
-    lateinit var name: String
-    lateinit var description: String
-    lateinit var gender: String
-    lateinit var birthday: Date
+    var user: User = User()
 
     init {
         getData()
     }
 
-    private fun getData()
-    {
+    private fun getData() {
         FirebaseFirestore.getInstance()
-            .collection(FirebaseAuth.getInstance().currentUser?.email.toString())
-            .document("infor")
+            .collection("user")
+            .document(FirebaseAuth.getInstance().currentUser?.email.toString())
             .get()
             .addOnSuccessListener {
-                name = it.get("name").toString()
-                description = it.get("description").toString()
-                gender = it.get("gender").toString()
-                name = it.get("name").toString()
+                user.getData(it)
             }
     }
 
     val nameTextWatcher: TextWatcher
-        get()=object :TextWatcher
-        {
-            override fun beforeTextChanged(s: CharSequence?, p1: Int, p2: Int, p3: Int) {
+        get() = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, p1: Int, p2: Int, p3: Int) {}
 
-            }
-
-            override fun onTextChanged(s: CharSequence?, p1: Int, p2: Int, p3: Int) {
-
-            }
+            override fun onTextChanged(s: CharSequence?, p1: Int, p2: Int, p3: Int) {}
 
             override fun afterTextChanged(s: Editable?) {
-                name = s.toString()
+                user.name = s.toString()
             }
 
         }
 
     val descriptionTextWatcher: TextWatcher
-        get()=object :TextWatcher
-        {
-            override fun beforeTextChanged(s: CharSequence?, p1: Int, p2: Int, p3: Int) {
+        get() = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, p1: Int, p2: Int, p3: Int) {}
 
-            }
-
-            override fun onTextChanged(s: CharSequence?, p1: Int, p2: Int, p3: Int) {
-
-            }
+            override fun onTextChanged(s: CharSequence?, p1: Int, p2: Int, p3: Int) {}
 
             override fun afterTextChanged(s: Editable?) {
-                description = s.toString()
+                user.description = s.toString()
             }
 
         }
 
-    fun updateName()
-    {
+    fun updateInfo() {
         FirebaseFirestore.getInstance()
-            .collection(FirebaseAuth.getInstance().currentUser?.email.toString())
-            .document("infor")
-            .update("name", name)
-    }
-
-    fun updateDescription()
-    {
-        FirebaseFirestore.getInstance()
-            .collection(FirebaseAuth.getInstance().currentUser?.email.toString())
-            .document("infor")
-            .update("description", description)
-    }
-
-    fun updateGender()
-    {
-        FirebaseFirestore.getInstance()
-            .collection(FirebaseAuth.getInstance().currentUser?.email.toString())
-            .document("infor")
-            .update("gender", gender)
+            .collection("user")
+            .document(FirebaseAuth.getInstance().currentUser?.email.toString())
+            .update("info", user)
     }
 }
