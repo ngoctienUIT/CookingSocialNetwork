765755bf8eca013adc66c2ae7aebb2d81984427e
AnBuiii <builehoaian2002@gmail.com>
2022-06-10 23:13:57 +0700

    Up tí

--- app/src/main/java/com/example/cookingsocialnetwork/post2/PostPage2TabFragment/PostPage2Fragment2.kt
+++ app/src/main/java/com/example/cookingsocialnetwork/post2/PostPage2TabFragment/PostPage2Fragment2.kt
@@ -1,20 +1,23 @@
 package com.example.cookingsocialnetwork.post2.PostPage2TabFragment
 
 import android.app.Dialog
-import android.content.Intent
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.*
-import android.widget.TextView
-import androidx.appcompat.app.AppCompatActivity
+import android.widget.EditText
 import androidx.fragment.app.Fragment
 import com.example.cookingsocialnetwork.R
-import com.example.cookingsocialnetwork.databinding.LayoutBottomsheetBinding
-import com.example.cookingsocialnetwork.mainActivity.MainActivity
-import com.example.cookingsocialnetwork.model.LanguageManager
+import com.example.cookingsocialnetwork.databinding.PortionPickerBinding
+import com.example.cookingsocialnetwork.databinding.TimePickerBinding
 
 class PostPage2Fragment2 : Fragment(){
+    private lateinit var portion: EditText
+    private lateinit var prepTime: EditText
+    private lateinit var bakingTime :EditText
+    private lateinit var restTime : EditText
+    private lateinit var portionType: Array<String>
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -26,29 +29,72 @@ class PostPage2Fragment2 : Fragment(){
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val assd = getView()?.findViewById<TextView>(R.id.post_page2_fragment2_food_portion)
-        assd?.setOnClickListener(){
-            showDialog()
+
+        portionType = resources.getStringArray(R.array.portion_type)
+
+        portion = requireView().findViewById(R.id.post_page2_fragment2_food_portion)
+        portion.setOnClickListener{
+            portionPicker()
         }
 
+        prepTime = requireView().findViewById(R.id.post_page2_fragment2_food_prep)
+        prepTime.setOnClickListener{
+            timePicker(prepTime)
+        }
+        bakingTime = requireView().findViewById(R.id.post_page2_fragment2_food_baking)
+        bakingTime.setOnClickListener{
+            timePicker(bakingTime)
+        }
+        restTime = requireView().findViewById(R.id.post_page2_fragment2_food_rest)
+        restTime.setOnClickListener{
+            timePicker(restTime)
+        }
     }
-    private fun showDialog()
-    {
+    private fun portionPicker() {
         val dialog = context?.let { Dialog(it) }
-        val dialogBinding: LayoutBottomsheetBinding = com.example.cookingsocialnetwork.databinding.LayoutBottomsheetBinding.inflate(layoutInflater)
+        val dialogBinding: PortionPickerBinding = PortionPickerBinding.inflate(layoutInflater)
         dialog!!.requestWindowFeature(Window.FEATURE_NO_TITLE)
         dialog.setContentView(dialogBinding.root)
 
+        dialogBinding.numberPortion.minValue = 1
+        dialogBinding.numberPortion.maxValue = 100
 
+        dialogBinding.portionType.minValue = 0
+        dialogBinding.portionType.maxValue = 1
+        dialogBinding.portionType.displayedValues = portionType
 
-
+        dialogBinding.portionPickerDoneBtn.setOnClickListener{
+            portion.setText(dialogBinding.numberPortion.value.toString() + " " +portionType[dialogBinding.portionType.value]  )
+            dialog.dismiss()
+        }
 
         dialog.window?.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
-        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.WHITE))
         dialog.window?.attributes?.windowAnimations = R.style.DialogAnimation
         dialog.window?.setGravity(Gravity.BOTTOM)
         dialog.show()
     }
+    private fun timePicker(editText: EditText){
+        val dialog = context?.let { Dialog(it) }
+        val dialogBinding: TimePickerBinding = TimePickerBinding.inflate(layoutInflater)
+        dialog!!.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setContentView(dialogBinding.root)
 
+        dialogBinding.timePickerHour.minValue = 0
+        dialogBinding.timePickerHour.maxValue = 72
 
+        dialogBinding.timePickerMinute.minValue = 0
+        dialogBinding.timePickerMinute.maxValue = 59
+
+        dialogBinding.timePickerDoneBtn.setOnClickListener{
+            editText.setText((dialogBinding.timePickerHour.value * 60 + dialogBinding.timePickerMinute.value).toString() + " phút")
+            dialog.dismiss()
+        }
+
+        dialog.window?.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.WHITE))
+        dialog.window?.attributes?.windowAnimations = R.style.DialogAnimation
+        dialog.window?.setGravity(Gravity.BOTTOM)
+        dialog.show()
+    }
 }
--- app/src/main/java/com/example/cookingsocialnetwork/post2/PostPage2TabFragment/PostPage2Fragment3.kt
+++ app/src/main/java/com/example/cookingsocialnetwork/post2/PostPage2TabFragment/PostPage2Fragment3.kt
@@ -1,21 +1,24 @@
 package com.example.cookingsocialnetwork.post2.PostPage2TabFragment
 
+import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.widget.Button
+import android.widget.LinearLayout
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.cookingsocialnetwork.R
+import com.example.cookingsocialnetwork.databinding.TimePickerBinding
+import com.google.android.material.button.MaterialButton
+import com.google.type.LatLng
 
 
 class PostPage2Fragment3 : Fragment() {
-    // TODO: Rename and change types of parameters
+    private lateinit var addIngredient: MaterialButton
+    private lateinit var ingredients: LinearLayout
 
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,7 +28,35 @@ class PostPage2Fragment3 : Fragment() {
         return inflater.inflate(R.layout.post_page_2_fragment3, container, false)
     }
 
-    companion object {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        ingredients = requireView().findViewById(R.id.post_page2_fragment3_ingredients)
+
+        addIngredient = requireView().findViewById(R.id.post_page2_fragment3_ingredients_btn)
+        addIngredient.setOnClickListener{
+//            val newLayout = LayoutInflater.from(context).inflate(R.layout.postpage_ingredient_child, null)
+//            ingredients.addView(newLayout)
+//            newLayout.findViewById<Button>(R.id.btn_Ingredients).setOnClickListener {
+//                (newLayout.parent as LinearLayout).removeView(newLayout)
+//            }
+            addIngredient()
+        }
     }
+    private fun addIngredient(){
+        val dialog = context?.let { Dialog(it) }
+        val dialogBinding: TimePickerBinding = TimePickerBinding.inflate(layoutInflater)
+        dialog!!.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setContentView(dialogBinding.root)
+
+
+
+        dialog.window?.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
+        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.WHITE))
+        dialog.window?.attributes?.windowAnimations = R.style.DialogAnimation
+        dialog.window?.setGravity(Gravity.BOTTOM)
+        dialog.show()
+    }
+
+
 }
