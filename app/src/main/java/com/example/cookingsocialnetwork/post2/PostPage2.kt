53035f9a54ae04ab70ba2a726fc5f782922f5d24
AnBuiii <builehoaian2002@gmail.com>
2022-06-11 23:50:23 +0700

    Update tí

--- app/src/main/java/com/example/cookingsocialnetwork/post2/PostPage2.kt
+++ app/src/main/java/com/example/cookingsocialnetwork/post2/PostPage2.kt
@@ -1,8 +1,11 @@
 package com.example.cookingsocialnetwork.post2
 
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import android.widget.RelativeLayout
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager2.widget.ViewPager2
@@ -14,7 +17,10 @@ import com.google.android.material.tabs.TabLayoutMediator
 class  PostPage2 : AppCompatActivity() {
     private lateinit var tabLayout: TabLayout
     private lateinit var viewPager: ViewPager2
+    private lateinit var backBtn: Button
     private lateinit var closeBtn: Button
+    private lateinit var previewBtn :Button
+    private lateinit var nextBtn: Button
     private val postPageAdapter = PostPage2ViewPagerAdapter(supportFragmentManager, lifecycle)
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,17 +29,55 @@ class  PostPage2 : AppCompatActivity() {
         setContentView(R.layout.activity_post_page_2)
 
         tabLayout = findViewById(R.id.post_page_tab)
+        backBtn = findViewById(R.id.post_page_btn_back)
         viewPager  = findViewById(R.id.post_page_viewPager)
+        closeBtn = findViewById(R.id.post_page_btn_close)
+        previewBtn = findViewById(R.id.post_page_preview_btn)
+        nextBtn = findViewById(R.id.post_page_next_button)
+
 
         viewPager.adapter = postPageAdapter
-
+        viewPager.registerOnPageChangeCallback(
+            object: ViewPager2.OnPageChangeCallback(){
+                override fun onPageSelected(position: Int) {
+                    if(position == 0){
+                        backBtn.text = "x"
+                    }
+                    else if( position == 4 ){
+                        previewBtn.visibility = View.GONE
+                        nextBtn.text = "Xem lại trước khi đăng"
+                    } else {
+                        backBtn.text = "<"
+                        nextBtn.text = "Tiếp theo"
+                        previewBtn.visibility = View.VISIBLE
+                    }
+                    super.onPageSelected(position)
+                }
+            }
+        )
+        viewPager.isUserInputEnabled = false
         TabLayoutMediator(tabLayout, viewPager){
-            tabLayout, position ->
+            tabLayoutt, position ->
         }.attach()
 
-        closeBtn = findViewById(R.id.post_page_btn_close)
+
         closeBtn.setOnClickListener {
-            println(postPageAdapter.fragment2.portion.text)
+            finish()
+        }
+
+        nextBtn.setOnClickListener {
+            val tab: TabLayout.Tab? = tabLayout.getTabAt(tabLayout.selectedTabPosition + 1)
+            tab?.select()
+        }
+        backBtn.setOnClickListener {
+            if(tabLayout.selectedTabPosition == 0){
+                finish()
+            }else {
+                val tab: TabLayout.Tab? = tabLayout.getTabAt(tabLayout.selectedTabPosition - 1)
+                tab?.select()
+            }
         }
     }
 }
+
+
